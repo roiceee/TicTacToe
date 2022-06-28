08b5864aff6a04e81a77f2293f36f33c9094b1f0
jroiceee <jroicealdeza@gmail.com>
2022-06-29 00:49:06 +0800

    finish ui

---
+++ script.js
@@ -0,0 +1,13 @@
+// ADD FOR PVP MODE
+
+// player chooses the cell
+// register the cell to the array
+// check winning conditions.
+//      if win, display winner and restore the whole board events
+//      RESTORE
+// display mark on cell
+// disable activity on that cell
+
+//WINNING CONDITION CHECK
+//the board is represented implemented using an array with the check method. loop the array and check for winning conditions diagonally, horizontally, and vertically
+
